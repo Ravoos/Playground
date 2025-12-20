c7d50fc362e79b7ee580d1a86e3a1db8d7063329
Martin <martin_lenart@icloud.com>
2025-12-20 17:17:00 +0100

    Lesson02

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 using Playground.Lesson01;
+using Playground.Lesson02;
 
 Console.WriteLine("Hello Playground!");
 
-Main01.Entry();
+//Main01.Entry();
+Main02.Entry();
